758d66347f3bec4cd771f8b13ad5fa631296bf55
collijk <collijk@uw.edu>
2023-11-14 16:39:23 -0800

    Add read_file implementation

--- src/rasterra/_io.py
+++ src/rasterra/_io.py
@@ -1,6 +1,16 @@
+from pathlib import Path
+
+import rasterio
+
+from ._array import RasterArray
 
 
-def read_file(path: str) -> str:
+def read_file(path: str | Path) -> RasterArray:
     """Read a file and return its content as a string."""
-    with open(path, "r") as f:
-        return f.read()
+    with rasterio.open(path) as f:
+        return RasterArray(
+            f.read(),
+            transform=f.transform,
+            crs=f.crs,
+            nodata=f.nodata,
+        )
