5a1feddceaba261ac30365177e84e2a29700f4ad
collijk <collijk@uw.edu>
2023-11-15 09:21:01 -0800

    Better color scaling

--- src/rasterra/_plotting.py
+++ src/rasterra/_plotting.py
@@ -1,9 +1,11 @@
-from typing import Any
+from typing import Any, Union
 
 import affine
+import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.axes import Axes
-from rasterio.plot import show, show_hist
+from matplotlib.colors import Colormap
+from rasterio.plot import plotting_extent, show_hist
 
 
 class Plotter:
@@ -11,13 +13,33 @@ class Plotter:
         self._data = data
         self._transform = transform
 
-    def __call__(self, *_: Any, **kwargs: Any) -> Axes:
-        return show(
-            source=self._data,
-            with_bounds=True,
-            transform=self._transform,
-            **kwargs,
-        )
+    def __call__(
+        self,
+        ax: Union[Axes, None] = None,
+        cmap: str | Colormap = "viridis",
+        vmin: Union[float, None] = None,
+        vmax: Union[float, None] = None,
+        nbins: int = 20,
+        **kwargs: Any,
+    ) -> Axes:
+        # Contrast stretching by default
+        if vmin is None:
+            vmin = np.percentile(self._data, 2)
+        if vmax is None:
+            vmax = np.percentile(self._data, 98)
+
+        kwargs["extent"] = plotting_extent(self._data, self._transform)
+
+        show_plt = False
+        if not ax:
+            show_plt = True
+            ax = plt.gca()
+
+        ax.imshow(self._data, vmin=vmin, vmax=vmax, cmap=cmap, **kwargs)
+        if show_plt:
+            plt.show()
+
+        return ax
 
     def hist(self, *_: Any, **kwargs: Any) -> Axes:
         return show_hist(
