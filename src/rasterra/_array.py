75c0fa4683e5b217e7fb16b7af9106a6ac0171c2
collijk <collijk@uw.edu>
2023-11-14 16:38:48 -0800

    template update

--- src/rasterra/_array.py
+++ src/rasterra/_array.py
@@ -1,15 +1,13 @@
-from typing import Any, Union, Optional
+from typing import Any, Optional, Union
 
 import numpy as np
 from affine import Affine
-
-from rasterio.warp import calculate_default_transform, reproject, Resampling
+from rasterio.warp import Resampling, reproject
 
 _RESAMPLING_MAP = {data.name: data for data in Resampling}
 
 
 class RasterArray:
-
     def __init__(
         self,
         data: np.ndarray,
@@ -83,7 +81,7 @@ class RasterArray:
         self,
         new_crs: str,
         resampling_method: Union[str, Resampling] = "nearest",
-    ) -> 'RasterArray':
+    ) -> "RasterArray":
         """
         Reproject the raster to a new coordinate reference system.
 
@@ -111,80 +109,3 @@ class RasterArray:
         )
 
         return RasterArray(out[0], out_transform, new_crs, self.nodata)
-
-    def resample(
-        self,
-        scale_factor: float,
-        resampling_method: Union[str, Resampling] = 'nearest',
-    ) -> 'RasterArray':
-        """Resample the raster data to a higher or lower resolution.
-
-        Parameters
-        ----------
-        scale_factor
-            Factor to scale the raster data. Use a scale factor > 1 to increase
-            the resolution (upsample) and a scale factor < 1 to decrease the
-            resolution (downsample).
-        resampling_method
-            Resampling method to use during CRS transformation.
-
-        Returns
-        -------
-        RasterArray
-            A new RasterArray with resampled data.
-        """
-        if isinstance(resampling_method, str):
-            resampling_method = _RESAMPLING_MAP[resampling_method]
-
-        new_width = int(self._data.shape[1] / scale_factor)
-        new_height = int(self._data.shape[0] / scale_factor)
-        new_transform = self.transform * self.transform.scale(
-            (self._data.shape[1] / new_width),
-            (self._data.shape[0] / new_height),
-        )
-
-        out, out_transform = reproject(
-            source=self._data,
-            src_transform=self.transform,
-            src_crs=self.crs,
-            dst_transform=new_transform,
-            dst_crs=self.crs,
-            resampling=resampling_method,
-        )
-
-        return RasterArray(out[0], out_transform, self.crs, self.nodata)
-
-    def clip(self, bounds: tuple[float, float, float, float]) -> 'RasterArray':
-        """
-        Clip the raster to a bounding box.
-
-        Parameters
-        ----------
-        bounds
-            Tuple specifying (left, bottom, right, top).
-
-        Returns
-        -------
-        RasterArray
-            A new RasterArray with clipped data.
-        """
-
-        left, bottom, right, top = bounds
-        left, bottom = self.transform * (left, bottom)
-        right, top = self.transform * (right, top)
-
-        window = self._data.window(left, bottom, right, top)
-        out = self._data.read(window=window, masked=True)
-
-        new_transform = calculate_default_transform(
-            src_crs=self.crs,
-            dst_crs=self.crs,
-            width=out.shape[2],
-            height=out.shape[1],
-            left=left,
-            bottom=bottom,
-            right=right,
-            top=top,
-        )
-
-        return RasterArray(out[0], new_transform, self.crs, self.nodata)
