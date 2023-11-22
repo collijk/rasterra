674ecb1ec3e7e983329e220ccae7374fe9c8b67a
James Collins <collijk@uw.edu>
2023-11-22 14:18:51 -0800

    Formatting

--- src/rasterra/_plotting.py
+++ src/rasterra/_plotting.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.axes import Axes
 from matplotlib.colors import Colormap, Normalize
-from rasterio.plot import plotting_extent, show_hist
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from rasterio.plot import plotting_extent
 
 
 class Plotter:
@@ -36,12 +36,7 @@ class Plotter:
             show_plt = True
             ax = plt.gca()
 
-        im = ax.imshow(self._data, norm=norm, cmap=cmap, **kwargs)
-        if under_color is not None:
-            im.cmap.set_under(under_color)
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05)
-        ax.figure.colorbar(ax.images[0], cax=cax, orientation='vertical')
+        _make_image_plot(self._data, norm, cmap, under_color, ax, **kwargs)
 
         if show_plt:
             plt.show()
@@ -65,10 +60,12 @@ class Plotter:
 
         fig, axes = plt.subplots(figsize=(15, 15), ncols=2, nrows=2)
 
-        _make_hist_plot(data, nbins, 'Raw data', axes[0, 0])
-        _make_image_plot(data, Normalize(vmin=vmin, vmax=vmax), cmap, under_color, axes[1, 0])
+        _make_hist_plot(data, nbins, "Raw data", axes[0, 0])
+        _make_image_plot(
+            data, Normalize(vmin=vmin, vmax=vmax), cmap, under_color, axes[1, 0]
+        )
 
-        _make_hist_plot(result, nbins, 'Normalized data', axes[0, 1])
+        _make_hist_plot(result, nbins, "Normalized data", axes[0, 1])
         _make_image_plot(result, norm, cmap, under_color, axes[1, 1])
 
         plt.show()
@@ -84,12 +81,12 @@ def _make_hist_plot(
     hist, bins = np.histogram(data, nbins)
     cdf = hist.cumsum()
     cdf = cdf / cdf.max()
-    ax.bar(bins[:-1], hist, width=bins[1:] - bins[:-1], color='tab:blue')
-    ax.set_ylabel('Frequency', color='tab:blue', fontsize=14)
+    ax.bar(bins[:-1], hist, width=bins[1:] - bins[:-1], color="tab:blue")
+    ax.set_ylabel("Frequency", color="tab:blue", fontsize=14)
     ax.set_title(title, fontsize=16)
     ax2 = ax.twinx()
-    ax2.plot(bins[:-1], cdf, color='tab:orange')
-    ax2.set_ylabel('Cumulative frequency', color='tab:orange', fontsize=14)
+    ax2.plot(bins[:-1], cdf, color="tab:orange")  # type: ignore[attr-defined]
+    ax2.set_ylabel("Cumulative frequency", color="tab:orange", fontsize=14)
     return ax
 
 
@@ -104,10 +101,10 @@ def _make_image_plot(
     """Plot a histogram of the data."""
     im = ax.imshow(data, cmap=cmap, norm=norm, **kwargs)
     if under_color is not None:
-        im.cmap.set_under(under_color)
+        im.cmap.set_under(under_color)  # type: ignore[union-attr]
 
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes('right', size='5%', pad=0.05)
-    ax.figure.colorbar(ax.images[0], cax=cax, orientation='vertical')
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    ax.figure.colorbar(ax.images[0], cax=cax, orientation="vertical")  # type: ignore[union-attr]
 
     return ax
